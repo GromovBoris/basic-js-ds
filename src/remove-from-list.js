abcad824a36141156e3143156c3ce906ac9e2833
GromovBoris <drunkgonzo@gmail.com>
2023-04-18 02:44:18 +0300

    feat: remove-from-list done

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -2,56 +2,49 @@ const { NotImplementedError } = require("../extensions/index.js");
 
 const { ListNode } = require("../extensions/list-node.js");
 
-/**
- * Given a singly linked list of integers l and an integer k,
- * remove all elements from list l that have a value equal to k.
- *
- * @param {List} l
- * @param {Number} k
- * @return {List}
- *
- * @example
- * For l = [3, 1, 2, 3, 4, 5] and k = 3,
- * the output should be [1, 2, 4, 5]
- *
- * Singly - linked lists are already defined using interface
- * class ListNode {
- *   constructor(x) {
- *     this.value = x;
- *     this.next = null;
- *   }
- * }
- */
-function removeKFromList(l, k) {
-  let array = Array.prototype.slice.call(l);
+//  * Given a singly linked list of integers l and an integer k,
+//  * remove all elements from list l that have a value equal to k.
 
-  for (let i = 0; i < array.length; i++) {
-    if (array[i] === k) {
-      array.splice(i, 1);
+// @param {List} l
+// @param {Number} k
+// @return {List}
+
+// @example
+//  * For l = [3, 1, 2, 3, 4, 5] and k = 3,
+//  * the output should be [1, 2, 4, 5]
+//  *
+//  * Singly - linked lists are already defined using interface
+
+// class ListNode {
+//   constructor(x) {
+//     this.value = x;
+//     this.next = null;
+//   }
+// }
+
+function removeKFromList(list, k) {
+  if (!list) {
+    return null;
+  }
+
+  while (list && list.value === k) {
+    list = list.next;
+  }
+
+  let current = list;
+
+  while (current && current.next) {
+    if (current.next.value === k) {
+      current.next = current.next.next;
+    } else {
+      current = current.next;
     }
   }
 
-  class ListNode {
-    constructor(x) {
-      this.value = x;
-      this.next = null;
-    }
-  }
-
-  function convertArrayToList(arr) {
-    return arr.reverse().reduce((acc, cur) => {
-      if (acc) {
-        const node = new ListNode(cur);
-        node.next = acc;
-        return node;
-      }
-
-      return new ListNode(cur);
-    }, null);
-  }
-
-  return convertArrayToList(array);
+  return list;
 }
+
+removeKFromList();
 
 module.exports = {
   removeKFromList,
